79f863dc1c6efdb84112f33bd58e94b0e9c605db
vini <vinicius.turu@gmail.com>
2020-08-23 20:57:08 -0300

    Aula 10 - Finalizada | Movimentação do Inimigo voador

--- js/js.js
+++ js/js.js
@@ -10,6 +10,9 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
 
     var jogo = {};
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
+
     var TECLA = {
         W: 87,
         S: 83,
@@ -37,6 +40,7 @@ function start() { // Inicio da função start()
 
         movefundo();
         movejogador();
+        moveinimigo1();
 
     } // Fim da função loop()
 
@@ -80,6 +84,20 @@ function start() { // Inicio da função start()
         }
 
     } // fim da função movejogador()
+
+    function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+
+        }
+    } //Fim da função moveinimigo1()
 
 
 
