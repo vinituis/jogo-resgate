5ba149b0f0e0a5cc3fb7a35513981241cc888ab1
vini <vinicius.turu@gmail.com>
2020-08-23 22:19:03 -0300

    Aula 22 - Finalizado | Recomeçar o jogo e outros ajustes

--- js/js.js
+++ js/js.js
@@ -414,23 +414,31 @@ function start() { // Inicio da função start()
     } // Fim da função energia()
 
     //Função GAME OVER
-	function gameOver() {
-        fimdejogo=true;
+    function gameOver() {
+        fimdejogo = true;
         musica.pause();
         somGameover.play();
-        
+
         window.clearInterval(jogo.timer);
-        jogo.timer=null;
-        
+        jogo.timer = null;
+
         $("#jogador").remove();
         $("#inimigo1").remove();
         $("#inimigo2").remove();
         $("#amigo").remove();
-        
+
         $("#fundoGame").append("<div id='fim'></div>");
-        
-        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
-        } // Fim da função gameOver();
+
+        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' style='cursor: pointer;' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+    } // Fim da função gameOver();
 
 } // Fim da Função 
 
+//Reinicia o Jogo
+
+function reiniciaJogo() {
+    somGameover.pause();
+    $("#fim").remove();
+    start();
+
+} //Fim da função reiniciaJogo
