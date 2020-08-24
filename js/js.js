c2c547d32f463aa0d5d45bbbc9982624b09dff34
vini <vinicius.turu@gmail.com>
2020-08-23 21:12:36 -0300

    Aula 12 - Finalizada | Disparo

--- js/js.js
+++ js/js.js
@@ -9,6 +9,7 @@ function start() { // Inicio da função start()
 
     //Principais variáveis do jogo
 
+    var podeAtirar = true;
     var jogo = {};
     var velocidade = 5;
     var posicaoY = parseInt(Math.random() * 334);
@@ -83,7 +84,7 @@ function start() { // Inicio da função start()
 
         if (jogo.pressionou[TECLA.D]) {
 
-            //Chama função Disparo	
+            disparo(); //Chama função Disparo	
         }
 
     } // fim da função movejogador()
@@ -125,6 +126,39 @@ function start() { // Inicio da função start()
         }
 
     } // fim da função moveamigo()
+
+    function disparo() {
+
+        if (podeAtirar == true) {
+
+            podeAtirar = false;
+
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX = parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro = topo + 50;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top", topoTiro);
+            $("#disparo").css("left", tiroX);
+
+            var tempoDisparo = window.setInterval(executaDisparo, 25);
+
+        } //Fecha podeAtirar
+
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left", posicaoX + 15);
+
+            if (posicaoX > 900) {
+
+                window.clearInterval(tempoDisparo);
+                tempoDisparo = null;
+                $("#disparo").remove();
+                podeAtirar = true;
+
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
 
 } // Fim da Função 
 
