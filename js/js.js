5871e21faca7e2bafbcd4969577255e47b77b505
vini <vinicius.turu@gmail.com>
2020-08-23 22:02:22 -0300

    Aula 19 - Finalizada | Velocidade do inimigo aumenta

--- js/js.js
+++ js/js.js
@@ -208,6 +208,7 @@ function start() { // Inicio da função start()
 
         if (colisao3.length > 0) {
 
+            velocidade=velocidade+0.3;
             pontos = pontos + 100;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
